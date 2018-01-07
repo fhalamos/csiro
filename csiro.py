d68d79524c55b049511579716f85dfc4681cfef4
Felipe Alamos <felipealamos@gmail.com>
2018-01-06 23:18:18 -0300

    new forms

--- csiro.py
+++ csiro.py
@@ -8,12 +8,20 @@ cache['foo'] = 1
 @app.route('/')
 def start(): 
     if cache['foo']==1:
-    	cache['foo']=0
-    	return redirect("https://docs.google.com/forms/d/e/1FAIpQLSfqHTAOCVUAbW4xEN2dnL_lHjvjtTz5RBclJpHYPi4Wo5TFwg/viewform", code=302)
-    elif cache['foo']==0:
+    	cache['foo']=2
+    	return redirect("https://docs.google.com/forms/d/e/1FAIpQLSeoBILwagcyCB6mewN0k0Ss9VSfjYRWfyhZpgleEhufPI8Ryg/viewform", code=302)
+    elif cache['foo']==2:
+    	cache['foo']=3
+    	return redirect("https://docs.google.com/forms/d/e/1FAIpQLSeN-0nIRL-EwJUtVyO23fXbcInqN3Z2PAf4hAYFIEsAWBYeJQ/viewform", code=302)
+    elif cache['foo']==3:
+    	cache['foo']=4
+    	return redirect("https://docs.google.com/forms/d/e/1FAIpQLSc43vtJYsv12JrfPh4tu6Qtzmqxb2aa1DMLcysZjq2bAerVCg/viewform", code=302)
+    elif cache['foo']==4:
     	cache['foo']=1
-    	return redirect("https://docs.google.com/forms/d/e/1FAIpQLSeXMowAZqlmandnKxlrGjq5kzNp0sFwFhy9XqlvHvKGGUXPow/viewform", code=302)
- 
+    	return redirect("https://docs.google.com/forms/d/e/1FAIpQLSfbmEe-7eBfuQN_DL2HiWOxR_WCCvGjJrGQNARKLpcVA9zWTQ/viewform", code=302)    	
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True, host='0.0.0.0')
